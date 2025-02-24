29d735a6f6d74aee0bb948594cab7aa97d7adf69
Kirill Trezubov <kirill3zoff@gmail.com>
2025-02-24 15:14:28 +0500

    Aura-upd: Implement HiglightActor and UnHilightActor enemy

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,20 +3,34 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
 
 AAuraEnemy::AAuraEnemy()
 {
     PrimaryActorTick.bCanEverTick = true;
+
+    GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
 }
 
 void AAuraEnemy::HighlightActor()
 {
-    bHighlighted = true;
+    GetMesh()->SetRenderCustomDepth(true);
+    GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+    if (Weapon)
+    {
+        Weapon->SetRenderCustomDepth(true);
+        Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+    }
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-    bHighlighted = false;
+    GetMesh()->SetRenderCustomDepth(false);
+    if (Weapon)
+    {
+        Weapon->SetRenderCustomDepth(false);
+    }    
 }
 
 void AAuraEnemy::BeginPlay()
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -18,9 +18,6 @@ public:
     virtual void HighlightActor() override;
     virtual void UnHighlightActor() override;
 
-    UPROPERTY(BlueprintReadOnly)
-    bool bHighlighted = false;
-
 protected:
     virtual void BeginPlay() override;
 };
