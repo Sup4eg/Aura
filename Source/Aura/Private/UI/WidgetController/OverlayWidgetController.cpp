fa8a2a6ebdfb02becf8fa2fb7665456aacc0abf9
Kirill Trezubov <kirill3zoff@gmail.com>
2025-03-27 09:47:00 +0500

    Aura-upd: Replacing callbacks with lambdas

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -21,16 +21,28 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
     check(AbilitySystemComponent);
 
     AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetHealthAttribute())
-                          .AddUObject(this, &ThisClass::HealthChanged);
+                          .AddLambda([this](const FOnAttributeChangeData& Data)
+                          {
+                              OnHealthChanged.Broadcast(Data.NewValue);
+                          });
 
     AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxHealthAttribute())
-                          .AddUObject(this, &ThisClass::MaxHealthChanged);
+                          .AddLambda([this](const FOnAttributeChangeData& Data)
+                          {
+                              OnMaxHealthChanged.Broadcast(Data.NewValue);
+                          });
 
     AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetManaAttribute())
-                          .AddUObject(this, &ThisClass::ManaChanged);
+                          .AddLambda([this](const FOnAttributeChangeData& Data)
+                          {
+                              OnManaChanged.Broadcast(Data.NewValue);
+                          });
 
     AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxManaAttribute())
-                          .AddUObject(this, &ThisClass::MaxManaChanged);
+                          .AddLambda([this](const FOnAttributeChangeData& Data)
+                          {
+                              OnMaxManaChanged.Broadcast(Data.NewValue);
+                          });
 
     Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
         [this](const FGameplayTagContainer& AssetTags)
@@ -47,24 +59,4 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
             }
         }
         );
-}
-
-void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
-{
-    OnHealthChanged.Broadcast(Data.NewValue);
-}
-
-void UOverlayWidgetController::MaxHealthChanged(const FOnAttributeChangeData& Data) const
-{
-    OnMaxHealthChanged.Broadcast(Data.NewValue);
-}
-
-void UOverlayWidgetController::ManaChanged(const FOnAttributeChangeData& Data) const
-{
-    OnManaChanged.Broadcast(Data.NewValue);
-}
-
-void UOverlayWidgetController::MaxManaChanged(const FOnAttributeChangeData& Data) const
-{
-    OnMaxManaChanged.Broadcast(Data.NewValue);
 }
