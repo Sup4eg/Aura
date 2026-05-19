3c2087168cc162b2d3ce61193143dad69988dce9
Kirill Trezubov <kirill3zoff@gmail.com>
2026-05-19 09:35:08 +0200

    Aura-add: Extend FAuraGameplayEffectContext with deep copy functionality and TStructOpsTypeTraits for improved serialization and replication

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -15,9 +15,21 @@ public:
     
     virtual UScriptStruct* GetScriptStruct() const override
     {
-        return FGameplayEffectContext::StaticStruct();
+        return StaticStruct();
     }
 
+    virtual FAuraGameplayEffectContext* Duplicate() const
+    {
+        FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+        *NewContext = *this;
+        if (GetHitResult())
+        {
+            // Does a deep copy of the hit result
+            NewContext->AddHitResult(*GetHitResult(), true);
+        }
+        return NewContext;
+    }
+    
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
     
 protected:
@@ -26,4 +38,14 @@ protected:
     
     UPROPERTY()
     bool bIsCriticalHit = false;
+};
+
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+    enum
+    {
+        WithNetSerialize = true,
+        WithCopy = true
+    };
 };
