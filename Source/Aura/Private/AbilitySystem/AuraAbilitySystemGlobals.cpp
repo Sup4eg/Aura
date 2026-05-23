0f3fb2b5332b29f4ea0b4de2ea29a91dba99eb8f
Kirill Trezubov <kirill3zoff@gmail.com>
2026-05-23 20:28:26 +0200

    Aura-add: Create UAuraAbilitySystemGlobals to override AllocGameplayEffectContext with FAuraGameplayEffectContext and configure in DefaultGame.ini

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright Fireteam Interactive
+
+
+#include "AuraAbilitySystemGlobals.h"
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+    return new FAuraGameplayEffectContext();
+}
